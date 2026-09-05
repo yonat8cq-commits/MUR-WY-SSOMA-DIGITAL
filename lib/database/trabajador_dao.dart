4b5fbaa9f1928b4aa7dee78233c80fd75062f614
yonat8cq-commits <yonat8cq@gmail.com>
2026-09-04 22:57:52 -0500

    Mejorar DAO de trabajadores SSOMA

--- lib/database/trabajador_dao.dart
+++ lib/database/trabajador_dao.dart
@@ -1,9 +1,9 @@
 class TrabajadorDao {
   Future<void> create(Map<String, dynamic> trabajador) async {
-    // CRUD trabajadores SSOMA.
+    // CRUD SQLite trabajadores SSOMA.
   }
 
-  Future<List<Map<String, dynamic>>> getAll() async {
+  Future<List<Map<String, dynamic>>> findAll() async {
     return [];
   }
 }
