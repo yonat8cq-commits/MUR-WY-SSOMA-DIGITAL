abdea3b487e95e2ddee5914c4b2072491e38c574
yonat8cq-commits <yonat8cq@gmail.com>
2026-09-04 22:57:02 -0500

    Add worker data access layer

---
+++ lib/database/trabajador_dao.dart
@@ -0,0 +1,9 @@
+class TrabajadorDao {
+  Future<void> create(Map<String, dynamic> trabajador) async {
+    // CRUD trabajadores SSOMA.
+  }
+
+  Future<List<Map<String, dynamic>>> getAll() async {
+    return [];
+  }
+}
