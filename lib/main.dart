ac4bdd31681e42b252bc4426a556817c49f52ea4
yonat8cq-commits <yonat8cq@gmail.com>
2026-09-05 07:36:59 -0500

    Integrate application routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'routes/app_routes.dart';
 
 void main() {
   runApp(const MurWySsomaApp());
@@ -16,41 +17,8 @@ class MurWySsomaApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MUR WY SSOMA DIGITAL'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            _moduleCard('Personal Activo', Icons.people),
-            _moduleCard('Capacitaciones', Icons.school),
-            _moduleCard('Inspecciones F-SGI-04-01', Icons.assignment),
-            _moduleCard('Reportes PDF / Excel', Icons.picture_as_pdf),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _moduleCard(String title, IconData icon) {
-    return Card(
-      child: ListTile(
-        leading: Icon(icon),
-        title: Text(title),
-        trailing: const Icon(Icons.arrow_forward_ios),
-      ),
+      initialRoute: AppRoutes.home,
+      routes: AppRoutes.routes,
     );
   }
 }
