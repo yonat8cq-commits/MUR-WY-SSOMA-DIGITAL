963168d6040ec065120b319edc670c284e73a707
yonat8cq-commits <yonat8cq@gmail.com>
2026-09-04 22:56:58 -0500

    Add SQLite database foundation

---
+++ lib/database/app_database.dart
@@ -0,0 +1,9 @@
+class AppDatabase {
+  static final AppDatabase instance = AppDatabase._init();
+
+  AppDatabase._init();
+
+  Future<void> initialize() async {
+    // Inicialización SQLite preparada para tablas SSOMA.
+  }
+}
